5aead65da77c70013cfb5043149d76d5ca88db64
JoyLynn <joylynnious@gmail.com>
2015-08-22 21:32:01 +0300

    added methods to the class

--- app/src/main/java/moringaschool/todo/Task.java
+++ app/src/main/java/moringaschool/todo/Task.java
@@ -5,6 +5,18 @@ package moringaschool.todo;
  */
 public class Task extends ParseObject {
     public Task(){
-        
+
+    }
+    public boolean isCompleted(){
+        return getBoolean("completed");
+    }
+    public void setCompleted(boolean complete){
+        put("completed", complete);
+    }
+    public String getDescription(){
+        return getString("description");
+    }
+    public void setDescription(String description){
+        put("description", description);
     }
 }
